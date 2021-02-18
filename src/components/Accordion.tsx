965dc04d3c89a5bc626738bc2b49ab50bc0efe87
Alex <tarkovskiy.alexandr@gmail.com>
2021-02-18 11:01:51 +0200

    added faq

---
+++ src/components/Accordion.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import '../static/styles/accordion.scss';
+import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from '@material-ui/icons/Remove';
+import classNames from 'classnames';
+import AnimateHeight from 'react-animate-height';
+
+interface AccordionProps{
+    title: string;
+    text: string;
+}
+
+function Accordion({ title, text }: AccordionProps){
+    const [open, setOpen] = useState(false);
+
+    return(
+        <div className = "accordion-root">
+            <div className = 'accordion-header'>
+                <p className='accordion-header-title'>{ title }</p>
+                <div onClick = {() => setOpen(!open)} className = {classNames("accordion-btf", { 'accordion-btf-active':  open})}>
+                    { open? <RemoveIcon style = {{fontSize: 20, color: "#FFFFFF"}}/> : <AddIcon style = {{fontSize: 20}}/> }
+                </div>
+            </div>
+            <div className = 'accordion-body'>
+                <AnimateHeight
+                    duration={ 500 }
+                    height={ open? 'auto' : 20 }
+                >
+                    <p className = "accordion-text">{text}</p>
+                </AnimateHeight>
+            </div>
+        </div>
+    )
+}
+
+export default Accordion;
--- src/containers/Faq/index.tsx
+++ src/containers/Faq/index.tsx
@@ -2,12 +2,41 @@ import React from 'react';
 import DocumentTitle from 'react-document-title';
 import Navigation, { LocationEnum } from '../../components/Navigation';
 import Footer from '../../components/Footer';
+import '../../static/styles/faq.scss';
+import Accordion from '../../components/Accordion';
 
 function Faq(){
     return(
         <DocumentTitle title="Faq">
-            <div>
+            <div className = "faq-root">
                 <Navigation location = {LocationEnum.FAQ}/>
+                <div className = "faq-body">
+                    <p className='title'>Frequently Asked Questions</p>
+
+                    <div className = "accordion-wrap">
+                        <Accordion
+                            title = "Frequently asked questions"
+                            text = {`On the Free plan, if you have the option to edit messages, only the most recent version will remain. On paid plans, Workspace Owners or Admins can choose to keep a log of all edited messages. Visit your Workspace Settings to confirm which option your workspace has enabled. 
+                                On the Free plan, if you have the option to edit messages, only the most recent version will remain. On paid plans, Workspace Owners or Admins can choose to keep a log of all edited messages. Visit your Workspace Settings to confirm which option your workspace has enabled. 
+                                We are proud to exceed the industry standard when it comes to protecting your organization and we've outlined many of our security practices and certifications on our website.
+                            `}
+                        />
+                        <Accordion
+                            title = "Frequently asked questions"
+                            text = {`On the Free plan, if you have the option to edit messages, only the most recent version will remain. On paid plans, Workspace Owners or Admins can choose to keep a log of all edited messages. Visit your Workspace Settings to confirm which option your workspace has enabled. 
+                                On the Free plan, if you have the option to edit messages, only the most recent version will remain. On paid plans, Workspace Owners or Admins can choose to keep a log of all edited messages. Visit your Workspace Settings to confirm which option your workspace has enabled. 
+                                We are proud to exceed the industry standard when it comes to protecting your organization and we've outlined many of our security practices and certifications on our website.
+                            `}
+                        />
+                        <Accordion
+                            title = "Frequently asked questions"
+                            text = {`On the Free plan, if you have the option to edit messages, only the most recent version will remain. On paid plans, Workspace Owners or Admins can choose to keep a log of all edited messages. Visit your Workspace Settings to confirm which option your workspace has enabled. 
+                                On the Free plan, if you have the option to edit messages, only the most recent version will remain. On paid plans, Workspace Owners or Admins can choose to keep a log of all edited messages. Visit your Workspace Settings to confirm which option your workspace has enabled. 
+                                We are proud to exceed the industry standard when it comes to protecting your organization and we've outlined many of our security practices and certifications on our website.
+                            `}
+                        />
+                    </div>
+                </div>
                 <Footer/>
             </div>
         </DocumentTitle>
