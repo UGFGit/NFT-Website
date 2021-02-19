0fb31f72dd4e940ad7170c17dc46874fb89617cc
Alex <tarkovskiy.alexandr@gmail.com>
2021-02-19 11:59:34 +0200

    added filters

--- src/containers/Collection/index.tsx
+++ src/containers/Collection/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import DocumentTitle from 'react-document-title';
 import Navigation, { LocationEnum } from '../../components/Navigation';
 import '../../static/styles/collection.scss';
@@ -9,21 +9,39 @@ import { IApplication } from '../../interfaces/Application/application.interface
 import InfiniteScroll from 'react-infinite-scroller';
 import Card from './Card';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import classNames from 'classnames';
 
 const DEFAULT_PAGE_SIZE = 20;
 
+interface IState{
+    list: IApplication[];
+    load: boolean;
+    mimetype: string | null;
+}
+
 function Collection(){
-    const [list, setList] = useState<IApplication[]>([]);
-    const [load, setLoad] = useState(true);
+    const [state, setState] = useState<IState>({ list: [], load: true, mimetype: null });
+
+    const scrollRef = useRef(null);
 
     const loadApplication = async (pageNumber = 0) => {
-        const response = await fetch.post(APPLICATION, { pagination: { pageSize: DEFAULT_PAGE_SIZE, pageNumber }});
-        const { applications, pagination } = await response.json();
-        
-        if(applications.length === 0){
-            setLoad(false);
+        const response = await fetch.post(APPLICATION, { pagination: { pageSize: DEFAULT_PAGE_SIZE, pageNumber }, filters: { mimetype: state.mimetype } });
+        const { applications, pagination} = await response.json();
+        const list = [...state.list, ...applications];
+        const newState = { ...state };
+
+        if(list.length === pagination.total || applications.length === 0){
+            newState.load = false;
+            setState(newState);
         }
-        setList([...list, ...applications]);
+
+        setState({...newState, list});
+    }
+
+    const setFilter = (filter: string | null) => () => {
+        //@ts-ignore
+        scrollRef.current.pageLoaded = -1;
+        setState({ mimetype: filter, list: [], load: true});
     }
     
     return(
@@ -48,19 +66,34 @@ function Collection(){
                 <div className = "collection-explore-root">
                     <div className = 'collection-explore-wrap'>
                         <p className = "collection-explore-title">Explore</p>
+                        <div className = 'collection-explore-filters-wrap'>
+                            <button 
+                                className = {classNames("collection-explore-filter-btn", { "collection-explore-filter-btn-active": state.mimetype === null })} 
+                                onClick = {setFilter(null)}
+                            >All</button>
+                            <button 
+                                className = {classNames("collection-explore-filter-btn", { "collection-explore-filter-btn-active": state.mimetype === 'image' })} 
+                                onClick = {setFilter('image')}
+                            >Art</button>
+                            <button 
+                                className = {classNames("collection-explore-filter-btn", { "collection-explore-filter-btn-active": state.mimetype === 'audio' })} 
+                                onClick = {setFilter('audio')}
+                            >Music</button>
+                        </div>
 
                         <div className = "collection-explore-scroll-wrap">
                             <InfiniteScroll
+                                ref={scrollRef}
                                 pageStart={-1}
                                 loadMore={loadApplication}
-                                hasMore={load}
+                                hasMore={state.load}
                                 loader={<div className="collection-explore-scroll-loader" key={0}><CircularProgress size={100} thickness={5} /></div>}
                                 useWindow={false}
                                 className = "collection-explore-cards-wrap"
                             >
-                            {list.map((item) => (
+                            {state.list.map((item) => (
                                     <Card key = {item.id} {...item}/>
-                                ))}
+                            ))}
                             </InfiniteScroll>
                         </div>
                     </div>
