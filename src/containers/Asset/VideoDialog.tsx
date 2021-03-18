ee23dc22a757982a9035f3991bca5e22be2a23a5
Alex <tarkovskiy.alexandr@gmail.com>
2021-03-18 15:50:32 +0200

    update asset dialog

--- src/containers/Asset/VideoDialog.tsx
+++ src/containers/Asset/VideoDialog.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import MuiDialog from '@material-ui/core/Dialog';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import DialogContent from '@material-ui/core/DialogContent';
-import IconButton from '@material-ui/core/IconButton';
-import CloseIcon from '@material-ui/icons/Close';
 import { IAsset } from '../../interfaces/containers/Application/asset.interface';
 import { FILESTORE } from '../../constants/endpoints';
 import VideoPlayer from '../../components/VideoPlayer';
@@ -16,23 +12,14 @@ interface DialogProps{
 
 function Dialog({ open, onClose, asset}: DialogProps){
     return(
-        <MuiDialog open={open} >
-            <DialogTitle style = {{display: 'flex', flexDirection: 'row', justifyContent: 'flex-end'}}>
-                <IconButton onClick = {onClose}>
-                    <CloseIcon />
-                </IconButton>
-            </DialogTitle>
-            <DialogContent dividers style = {{
-                padding: "16px 24px"
-            }}>
-                <VideoPlayer
-                    src = {FILESTORE(asset.metadata.filename)}
-                    muted = {false}
-                    controls = {true}
-                    loop = {false}
-                    playing = {true}
-                />           
-            </DialogContent>
+        <MuiDialog open={open} onClose = {onClose} >
+            <VideoPlayer
+                src = {FILESTORE(asset.metadata.filename)}
+                muted = {false}
+                controls = {true}
+                loop = {false}
+                playing = {true}
+            />  
         </MuiDialog>
     )
 }
